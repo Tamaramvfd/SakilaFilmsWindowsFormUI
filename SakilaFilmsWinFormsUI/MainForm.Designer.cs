cc25325a7a37d3ce56cc1b7d06e0587b4d2c5d31
unknown <tamaramoya@paucasesnovescifp.cat>
2020-12-01 19:10:19 +0100

    paginación

--- SakilaFilmsWinFormsUI/MainForm.Designer.cs
+++ SakilaFilmsWinFormsUI/MainForm.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.filmsListView = new System.Windows.Forms.ListView();
             this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.filmsFoundLabel = new System.Windows.Forms.Label();
+            this.pagesFoundLabel = new System.Windows.Forms.Label();
+            this.filmsPerPageComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // filmsListView
@@ -37,7 +42,7 @@
             this.filmsListView.HideSelection = false;
             this.filmsListView.Location = new System.Drawing.Point(12, 96);
             this.filmsListView.Name = "filmsListView";
-            this.filmsListView.Size = new System.Drawing.Size(777, 342);
+            this.filmsListView.Size = new System.Drawing.Size(777, 268);
             this.filmsListView.TabIndex = 0;
             this.filmsListView.UseCompatibleStateImageBehavior = false;
             this.filmsListView.View = System.Windows.Forms.View.List;
@@ -51,17 +56,70 @@
             this.categoryComboBox.TabIndex = 1;
             this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.categoryComboBox_SelectedIndexChanged);
             // 
+            // prevButton
+            // 
+            this.prevButton.Location = new System.Drawing.Point(293, 388);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(75, 36);
+            this.prevButton.TabIndex = 2;
+            this.prevButton.Text = "Prev";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(398, 388);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 36);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // filmsFoundLabel
+            // 
+            this.filmsFoundLabel.AutoSize = true;
+            this.filmsFoundLabel.Location = new System.Drawing.Point(273, 67);
+            this.filmsFoundLabel.Name = "filmsFoundLabel";
+            this.filmsFoundLabel.Size = new System.Drawing.Size(76, 17);
+            this.filmsFoundLabel.TabIndex = 4;
+            this.filmsFoundLabel.Text = "films found";
+            // 
+            // pagesFoundLabel
+            // 
+            this.pagesFoundLabel.AutoSize = true;
+            this.pagesFoundLabel.Location = new System.Drawing.Point(441, 67);
+            this.pagesFoundLabel.Name = "pagesFoundLabel";
+            this.pagesFoundLabel.Size = new System.Drawing.Size(46, 17);
+            this.pagesFoundLabel.TabIndex = 5;
+            this.pagesFoundLabel.Text = "label1";
+            // 
+            // filmsPerPageComboBox
+            // 
+            this.filmsPerPageComboBox.FormattingEnabled = true;
+            this.filmsPerPageComboBox.Location = new System.Drawing.Point(658, 36);
+            this.filmsPerPageComboBox.Name = "filmsPerPageComboBox";
+            this.filmsPerPageComboBox.Size = new System.Drawing.Size(121, 24);
+            this.filmsPerPageComboBox.TabIndex = 6;
+            this.filmsPerPageComboBox.SelectedIndexChanged += new System.EventHandler(this.filmsPerPageComboBox_SelectedIndexChanged);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.filmsPerPageComboBox);
+            this.Controls.Add(this.pagesFoundLabel);
+            this.Controls.Add(this.filmsFoundLabel);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.prevButton);
             this.Controls.Add(this.categoryComboBox);
             this.Controls.Add(this.filmsListView);
             this.Name = "MainForm";
             this.Text = "Sakila Films";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +127,11 @@
 
         private System.Windows.Forms.ListView filmsListView;
         private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label filmsFoundLabel;
+        private System.Windows.Forms.Label pagesFoundLabel;
+        private System.Windows.Forms.ComboBox filmsPerPageComboBox;
     }
 }
 
--- SakilaFilmsWinFormsUI/MainForm.cs
+++ SakilaFilmsWinFormsUI/MainForm.cs
@@ -10,12 +10,18 @@ namespace SakilaFilmsWinFormsUI
 {
     public partial class MainForm : Form
     {
+        int filmsPerPage;
+        int numberOfPages;
+        int currentPages = 0;
         public MainForm()
         {
             InitializeComponent();
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
+            filmsFoundLabel.Text = "0 films found";
+            pagesFoundLabel.Text = "0 pages";
+            filmsPerPage = int.Parse(filmsPerPageComboBox.Text);
             //Establecer conexión con la BBDD
             string connectionString = ConfigurationManager.ConnectionStrings["SakilaDB"].ConnectionString;
 
@@ -51,6 +57,78 @@ namespace SakilaFilmsWinFormsUI
                     filmsListView.Items.Add(film.ToString());
                 }
             }
+
+            //string connectionString = ConfigurationManager.ConnectionStrings["SakilaDB"].ConnectionString;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string sql = $"SELECT COUNT(*) " +
+                    $"FROM " +
+                    $"film INNER JOIN film_category ON film.film_id = film_category.film_id " +
+                    $"INNER JOIN category ON film_category.category_id = category.category_id " +
+                    $"WHERE category.name = '{categoryComboBox.Text}'";
+
+                int totalNumberOfFilmsPerCategoryFound = connection.Query<int>(sql).FirstOrDefault();
+                filmsFoundLabel.Text = totalNumberOfFilmsPerCategoryFound + "films found";
+
+                numberOfPages = totalNumberOfFilmsPerCategoryFound / int.Parse(filmsPerPageComboBox.Text) + 1;
+                currentPages = 0;
+                prevButton.Enabled = false;
+                nextButton.Enabled = true;
+            }
         }
-    }
+
+        private void filmsPerPageComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filmsPerPage = int.Parse(filmsPerPageComboBox.Text);
+            int lastSelectedIndex = categoryComboBox.SelectedIndex;
+            categoryComboBox.SelectedIndex = -1;
+            categoryComboBox.SelectedIndex = lastSelectedIndex;
+        }
+
+        private void UpdateFilmsView()
+        {
+            pagesFoundLabel.Text = (currentPages + 1) + "of" + numberOfPages + " pages";
+
+            string connectionString = ConfigurationManager.ConnectionStrings["SakilaDB"].ConnectionString;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string sql = $"SELECT film.film_id AS Id, film.title, film.description, film.length " +
+                    $"FROM film " +
+                    $"INNER JOIN film_category ON film.film_id = film_category.film_id " +
+                    $"INNER JOIN category ON film_category.category_id = category.category_id " +
+                    $"WHERE category.name = '{categoryComboBox.Text}' " +
+                    $"LIMIT {currentPages * filmsPerPage}, {filmsPerPage}";
+
+                List<FilmModel> films = new List<FilmModel>();
+                films = connection.Query<FilmModel>(sql).ToList();
+                filmsListView.Items.Clear();
+                foreach (FilmModel film in films)
+                {
+                    filmsListView.Items.Add(film.ToString());
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            currentPages++;
+            if (currentPages == numberOfPages - 1)
+            {
+                nextButton.Enabled = false;
+            }
+            prevButton.Enabled = true;
+            UpdateFilmsView();
+        }
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            currentPages--;
+            if (currentPages == 0)
+            {
+                prevButton.Enabled = false;
+            }
+            nextButton.Enabled = true;
+            UpdateFilmsView();
+        }
+    }    
 }
