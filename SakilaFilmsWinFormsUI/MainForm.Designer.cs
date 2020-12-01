dac46a1d2248b6c65721f689280364de76dd0f30
unknown <tamaramoya@paucasesnovescifp.cat>
2020-12-01 18:26:42 +0100

    Initial commit

---
+++ SakilaFilmsWinFormsUI/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SakilaFilmsWinFormsUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filmsListView = new System.Windows.Forms.ListView();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // filmsListView
+            // 
+            this.filmsListView.HideSelection = false;
+            this.filmsListView.Location = new System.Drawing.Point(12, 96);
+            this.filmsListView.Name = "filmsListView";
+            this.filmsListView.Size = new System.Drawing.Size(777, 342);
+            this.filmsListView.TabIndex = 0;
+            this.filmsListView.UseCompatibleStateImageBehavior = false;
+            this.filmsListView.View = System.Windows.Forms.View.List;
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(12, 36);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(202, 24);
+            this.categoryComboBox.TabIndex = 1;
+            this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.categoryComboBox_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.filmsListView);
+            this.Name = "MainForm";
+            this.Text = "Sakila Films";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView filmsListView;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+    }
+}
+
---
+++ SakilaFilmsWinFormsUI/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using Dapper;
+using System.Linq;
+
+namespace SakilaFilmsWinFormsUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Establecer conexión con la BBDD
+            string connectionString = ConfigurationManager.ConnectionStrings["SakilaDB"].ConnectionString;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string sql = "SELECT name FROM sakila.category";
+                List<string> categories = new List<string>();
+                categories = connection.Query<string>(sql).ToList();
+                foreach (string category in categories)
+                {
+                    categoryComboBox.Items.Add(category);
+                }
+            }
+        }
+
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SakilaDB"].ConnectionString;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string sql = $"SELECT film.film_id, film.title, film.description, film.length " +
+                    $"FROM +" +
+                    $"film INNER JOIN film_category ON film.film.id = film_category.film_id " +
+                    $"INNER JOIN category ON film_category.category_id = category.category_id " +
+                    $"WHERE category.name = '{categoryComboBox.Text}'";
+
+                List<FilmModel> films = new List<FilmModel>();
+                films = connection.Query<FilmModel>(sql).ToList();
+                foreach (FilmModel film in films)
+                {
+                    //filmsListView.Items.Add($"{film.Id}, {film.Title}, {film.Description}, {film.Length}");
+                    filmsListView.Items.Add(film.ToString());
+                }
+            }
+        }
+    }
+}
